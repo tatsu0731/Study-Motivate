423b120fa1fb8e0fb685412742336ae20136d759
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-05-24 01:50:35 +0900

    add: モーダルを追加

--- front/src/Components/Atoms/Button.tsx
+++ front/src/Components/Atoms/Button.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-export default function Button({title}) {
+export default function Button({title, onClick}) {
     return (
-        <button className="px-12 py-2 bg-red-400 text-white font-bold rounded-full text-sm shadow">{title}</button>
+        <button className="px-12 py-2 bg-red-400 text-white font-bold rounded-full text-sm shadow" onClick={onClick}>{title}</button>
     );
 }
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -3,14 +3,38 @@ import GrowImages from "@/Components/Atoms/GrowImages";
 import StatusBox from "@/Components/Organism/StatusBox";
 import Footer from "@/Components/Templetes/Footer";
 import Header from "@/Components/Templetes/Header";
+import { useState } from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement(".App");
+
+const modalStyle = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.85)"
+  },
+  content: {
+    position: "absolute",
+    top: "5rem",
+    left: "5rem",
+    right: "5rem",
+    bottom: "5rem",
+    backgroundColor: "white",
+    borderRadius: "1rem",
+    padding: "1.5rem"
+  }
+};
 
 const Data = {
   Hour: 5,
 }
 
 export default function Home() {
+  const [modalIsOpen, setIsOpen] = useState(false);
   return (
-    <>
+    <div className="App">
       <Header />
       <main className="flex justify-center">
         <div>
@@ -19,14 +43,17 @@ export default function Home() {
             <StatusBox />
           </section>
           <section className="justify-center flex mt-20">
-            <Button title={"今日の勉強時間を入力しましょう！"}/>
+            <Button title={"今日の勉強時間を入力しましょう！"} onClick={() => setIsOpen(true)}/>
           </section>
+          <Modal isOpen={modalIsOpen} style={modalStyle}>
+            <button onClick={() => setIsOpen(false)}>✖︎</button>
+          </Modal>
           {/* ここら辺に今日の入力が終わったらお疲れ様でした！みたいな表記を出したい */}
         </div>
       </main>
       <div className="flex justify-center">
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
